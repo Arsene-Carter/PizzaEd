949561494454144022d1098a2624d05eb5a98cd4
Arsene <shyakagasanaarsene@gmail.com>
2021-11-07 12:02:28 +0100

    added a new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+      "new-pizza":{
+        'ingredients':{'mozzarella':2,'cheese':2,'chicken':4},
+        'price':18
     }
 }
